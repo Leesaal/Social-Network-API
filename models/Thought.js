2115bef18324c2b59c6b1b284a518921dd202ba2
Leesa A <leesaali7@gmail.com>
2023-07-13 00:51:29 +0100

    add Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,3 +1,34 @@
 const { Schema, model } = require("mongoose");
 
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 250,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("thoguht", thoughtSchema);
+
 module.exports = Thought;
